112b863386d60977b4938feb9c2ef5ebe921d3e0
bioback <78535998+bioback@users.noreply.github.com>
2021-11-17 15:04:34 +0100

    renders only if product is defined

--- src/screens/ProductDetail.js
+++ src/screens/ProductDetail.js
@@ -84,138 +84,140 @@ const ProductDetails = ({ match, history }) => {
         <h2>{error}</h2>
       ) : (
         <>
-          <div className="row">
-            <Helmet>
-              <title>{`Cavalieri Suits | ${product.name}`}</title>
-              <meta name="description" content={`${product.description}`} />
-            </Helmet>
-            <div className="productdetails__left col-12 col-lg-5 my-auto">
-              <div className="left__image">
-                <Carousel
-                  // activeIndex={index}
-                  // onSelect={handleSelect}
-                  nextLabel=""
-                  prevLabel=""
-                >
-                  {product.imageUrl.map((i, index) => (
-                    <Carousel.Item key={index}>
-                      <img
-                        className="d-block w-100"
-                        src={i}
-                        alt={index}
-                        data-bs-toggle="modal"
-                        data-bs-target={`#deliverModal${index}`}
-                      />
-                    </Carousel.Item>
-                  ))}
-                </Carousel>
-                {product.imageUrl.map((i, index) => (
-                  <div
-                    className="modal fade"
-                    id={`deliverModal${index}`}
-                    key={index}
-                    tabIndex="-1"
-                    aria-labelledby={`deliverModal${index}`}
-                    aria-hidden="true"
+          {product && (
+            <div className="row">
+              <Helmet>
+                <title>{`Cavalieri Suits | ${product.name}`}</title>
+                <meta name="description" content={`${product.description}`} />
+              </Helmet>
+              <div className="productdetails__left col-12 col-lg-5 my-auto">
+                <div className="left__image">
+                  <Carousel
+                    // activeIndex={index}
+                    // onSelect={handleSelect}
+                    nextLabel=""
+                    prevLabel=""
                   >
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-body p-1">
-                          <img className="modal-img" src={i} alt={i} />
+                    {product.imageUrl.map((i, index) => (
+                      <Carousel.Item key={index}>
+                        <img
+                          className="d-block w-100"
+                          src={i}
+                          alt={index}
+                          data-bs-toggle="modal"
+                          data-bs-target={`#deliverModal${index}`}
+                        />
+                      </Carousel.Item>
+                    ))}
+                  </Carousel>
+                  {product.imageUrl.map((i, index) => (
+                    <div
+                      className="modal fade"
+                      id={`deliverModal${index}`}
+                      key={index}
+                      tabIndex="-1"
+                      aria-labelledby={`deliverModal${index}`}
+                      aria-hidden="true"
+                    >
+                      <div className="modal-dialog">
+                        <div className="modal-content">
+                          <div className="modal-body p-1">
+                            <img className="modal-img" src={i} alt={i} />
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
-            <div className="productdetails__right d-flex row justify-content-evenly my-5 col-12 col-lg-7">
-              <Container className="left__info px-5 py-2">
-                <h2 className="left__name fw-bold fs-1">{product.name}</h2>
-                <p>{product.description}</p>
-              </Container>
-              <Container className="right__info px-5 py-2">
-                <p>
-                  Precio: <span>${product.price}</span>
-                </p>
-                <p>
-                  Disponibilidad:{" "}
-                  <strong>
-                    {product.countInStock > 0 ? "En Stock" : "Agotado"}
-                  </strong>
-                </p>
-                <p>
-                  Qty
-                  <select
-                    className="mx-2"
-                    value={qty}
-                    onChange={(e) => setQty(e.target.value)}
-                  >
-                    {[...Array(product.countInStock).keys()].map((x) => (
-                      <option key={x + 1} value={x + 1}>
-                        {x + 1}
-                      </option>
-                    ))}
-                  </select>
-                </p>
-                <div className="row justify-content-between">
-                  <div className="col-12 my-2 col-lg-6">
-                    <Button
-                      className="col-9"
-                      variant="dark"
-                      onClick={addToCartHandler}
+              <div className="productdetails__right d-flex row justify-content-evenly my-5 col-12 col-lg-7">
+                <Container className="left__info px-5 py-2">
+                  <h2 className="left__name fw-bold fs-1">{product.name}</h2>
+                  <p>{product.description}</p>
+                </Container>
+                <Container className="right__info px-5 py-2">
+                  <p>
+                    Precio: <span>${product.price}</span>
+                  </p>
+                  <p>
+                    Disponibilidad:{" "}
+                    <strong>
+                      {product.countInStock > 0 ? "En Stock" : "Agotado"}
+                    </strong>
+                  </p>
+                  <p>
+                    Qty
+                    <select
+                      className="mx-2"
+                      value={qty}
+                      onChange={(e) => setQty(e.target.value)}
                     >
-                      <box-icon
-                        type="regular"
-                        color="white"
-                        size="sm"
-                        name="cart"
-                        animation="tada-hover"
-                      />
-                      <span className="ms-2">Agregar al carrito</span>
-                    </Button>
-                  </div>
-                  <div className="col-12 my-2 col-lg-6">
-                    {isFavorite ? (
+                      {[...Array(product.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </select>
+                  </p>
+                  <div className="row justify-content-between">
+                    <div className="col-12 my-2 col-lg-6">
                       <Button
                         className="col-9"
-                        variant="secondary"
-                        onClick={removeFromFavorites}
-                      >
-                        <box-icon
-                          type="solid"
-                          color="white"
-                          size="sm"
-                          name="heart"
-                          animation="tada-hover"
-                        />
-                        <span className="ms-2">
-                          {load ? <></> : "En favoritos"}
-                        </span>
-                      </Button>
-                    ) : (
-                      <Button
-                        className="col-9"
-                        variant="secondary"
-                        onClick={addToFavorites}
+                        variant="dark"
+                        onClick={addToCartHandler}
                       >
                         <box-icon
                           type="regular"
                           color="white"
                           size="sm"
-                          name="heart"
+                          name="cart"
                           animation="tada-hover"
                         />
-                        <span className="ms-2">
-                          {load ? <></> : "Agregar a favoritos"}
-                        </span>
+                        <span className="ms-2">Agregar al carrito</span>
                       </Button>
-                    )}
+                    </div>
+                    <div className="col-12 my-2 col-lg-6">
+                      {isFavorite ? (
+                        <Button
+                          className="col-9"
+                          variant="secondary"
+                          onClick={removeFromFavorites}
+                        >
+                          <box-icon
+                            type="solid"
+                            color="white"
+                            size="sm"
+                            name="heart"
+                            animation="tada-hover"
+                          />
+                          <span className="ms-2">
+                            {load ? <></> : "En favoritos"}
+                          </span>
+                        </Button>
+                      ) : (
+                        <Button
+                          className="col-9"
+                          variant="secondary"
+                          onClick={addToFavorites}
+                        >
+                          <box-icon
+                            type="regular"
+                            color="white"
+                            size="sm"
+                            name="heart"
+                            animation="tada-hover"
+                          />
+                          <span className="ms-2">
+                            {load ? <></> : "Agregar a favoritos"}
+                          </span>
+                        </Button>
+                      )}
+                    </div>
                   </div>
-                </div>
-              </Container>
+                </Container>
+              </div>
             </div>
-          </div>
+          )}
         </>
       )}
     </Container>
